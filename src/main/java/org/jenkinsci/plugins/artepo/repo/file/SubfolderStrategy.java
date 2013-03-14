afdab618efdb2c88316fd950da685945cfdb98c1
czawadka <czawadka@gmail.com>
2013-03-14 20:11:36 +0100

    use subfolder strategy to implement variations of types of subfolders (Issue #5)

---
+++ src/main/java/org/jenkinsci/plugins/artepo/repo/file/SubfolderStrategy.java
@@ -0,0 +1,78 @@
+package org.jenkinsci.plugins.artepo.repo.file;
+
+import hudson.FilePath;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public enum SubfolderStrategy {
+
+    OnlyBuildSubfolders() {
+        @Override
+        public List<FilePath> getDestinationPaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    getFormattedBuildSubfolder(repoPath, buildNumber)
+            );
+        }
+
+        @Override
+        public List<FilePath> getPotentialSourcePaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    getFormattedBuildSubfolder(repoPath, buildNumber),
+                    getUnFormattedBuildSubfolder(repoPath, buildNumber)
+            );
+        }
+    },
+
+    BuildAndLatestSubfolders() {
+        @Override
+        public List<FilePath> getDestinationPaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    getFormattedBuildSubfolder(repoPath, buildNumber),
+                    repoPath.child("latest")
+            );
+        }
+
+        @Override
+        public List<FilePath> getPotentialSourcePaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    getFormattedBuildSubfolder(repoPath, buildNumber)
+            );
+        }
+    },
+
+    NoSubfolders() {
+        @Override
+        public List<FilePath> getDestinationPaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    repoPath
+            );
+        }
+
+        @Override
+        public List<FilePath> getPotentialSourcePaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException {
+            return Arrays.asList(
+                    repoPath
+            );
+        }
+    };
+
+    static public final SubfolderStrategy DEFAULT = OnlyBuildSubfolders;
+
+    abstract public List<FilePath> getDestinationPaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException;
+    abstract public List<FilePath> getPotentialSourcePaths(FilePath repoPath, int buildNumber) throws InterruptedException, IOException;
+
+
+    public FilePath getFormattedBuildSubfolder(FilePath repoPath, int buildNumber) {
+        return repoPath.child(formatBuildNumber(buildNumber));
+    }
+    public FilePath getUnFormattedBuildSubfolder(FilePath repoPath, int buildNumber) {
+        return repoPath.child(String.valueOf(buildNumber));
+    }
+
+    static public String formatBuildNumber(int buildNumber) {
+        return String.format("%05d", buildNumber);
+    }
+
+}
